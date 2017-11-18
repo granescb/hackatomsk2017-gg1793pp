344bacb0ab7e3e9843975e0c69b397c51cddfe62
Sergey <granescb@gmail.com>
2017-11-19 00:34:48 +0700

    added rooms

---
+++ server/models/roomModel.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectIdSchema = Schema.ObjectId,
+    ObjectId = mongoose.Types.ObjectId,
+    db = require('../model');
+
+var RoomModel = new Schema({
+    id: {type:ObjectIdSchema, default: function () { return new ObjectId()} },
+    maxCount: {
+        type: Number,
+        default: 2
+    },
+    userList : {
+        type: Array,
+        default: []
+    },
+    currentCount: {
+        type: Number,
+        default: 0
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 120
+    }
+});
+
+RoomModel.getNotFullRoom = function (person) {
+    this.findOne({'isActive': true}).where('maxCount').gt('currentCount').exec(
+        function (err, room) {
+            if (err) console.log('error '+err);
+            else{
+                console.log(room.isActive);
+            }
+        }
+    )
+};
+
+module.exports = db.model('Room', RoomModel);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    ObjectIdSchema = Schema.ObjectId,
+    ObjectId = mongoose.Types.ObjectId,
     db = require('../model');
-    // mongoose.createConnection('mongodb://localhost:27017/roulette');
 
 var UserModel = new Schema({
-    login: String,
+    id: {type:ObjectIdSchema, default: function () { return new ObjectId()} },
+    login: {type: String, unique: true},
     password: String,
     createdAt: {
         type: Date,
@@ -14,8 +16,15 @@ var UserModel = new Schema({
     balance: {
         type: Number,
         default: 0
-    }
+    },
+    currentRoom: ObjectIdSchema
 });
+UserModel.methods.create = function (login, password) {
+    var greeting = this.login
+        ? "My name is " + this.login
+        : "I don't have a name";
+    console.log(greeting);
+};
 UserModel.methods.speak = function () {
     var greeting = this.login
         ? "My name is " + this.login
