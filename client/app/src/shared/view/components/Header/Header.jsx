f3dc2f1c815a9e9641a9ec9ea29296bc06465880
Pavel <p.naydanovgoo@gmail.com>
2017-11-18 19:58:47 +0700

    final carcas

--- client/app/src/shared/view/components/Header/Header.jsx
+++ client/app/src/shared/view/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import block from 'bem-cn';
 import { Link } from 'react-router';
-import SVGInline from 'react-svg-inline';
 
 import logoImg from './img/logo.svg';
 import './Header.styl';
@@ -9,23 +8,15 @@ import './Header.styl';
 class Header extends React.Component {
 
   static propTypes = {
-
-    onMenuItemAuthBlockClick: PropTypes.func.isRequired,
   }
 
   render() {
     const b = block('header');
     return (
       <div className={b}>
-          <div className={b('img-container')}>
-            <SVGInline
-              svg={logoImg}
-              className={b('img')()}
-              classSuffix=""
-              onClick={this.onGoogleLoginClick}
-            />
-          </div>
         <Link className={b('wrapper', { position: 'left' })} to={'/roulette'}>
+          <img src={logoImg} alt="" />
+          <p className={b('text')}>Betting-software</p>
         </Link>
         <div className={b('wrapper', { position: 'center' })}></div>
         <div className={b('wrapper', { position: 'right' })}>
