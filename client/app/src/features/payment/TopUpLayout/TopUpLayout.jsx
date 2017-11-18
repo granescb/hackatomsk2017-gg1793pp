57953f4dac5d6e76d75f30ad3b433625e8373d92
mr_mki <mr_mki@protonmail.com>
2017-11-18 15:26:38 +0700

    create top level for payment

---
+++ client/app/src/features/payment/TopUpLayout/TopUpLayout.jsx
@@ -0,0 +1,75 @@
+import React, { Component, PropTypes } from 'react';
+import block from 'bem-cn';
+import { bind } from 'decko';
+import { actions, TopUpMethods } from 'features/payment/redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import './TopUpLayout.styl';
+import TopUpMethodBlock from './TopUpMethodBlock';
+import CardMethodBlock from './CardMethodBlock';
+
+class TopUpLayout extends Component {
+
+  static propTypes = {
+    actionProcessing: PropTypes.bool.isRequired,
+    errorMessage: PropTypes.string,
+    initBitapsPayment: PropTypes.func.isRequired,
+    onRequestError: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onRequestError: () => { console.error('Top Up Layout Request Error!'); },
+  };
+
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { actionProcessing, errorMessage,
+            onRequestError, initBitapsPayment } = nextProps;
+    if (this.props.actionProcessing && !actionProcessing) {
+      onRequestError(errorMessage);
+    }
+  }
+
+
+  @bind
+  onMethodTopUpClick(methodID, paymentInfo) {
+    const { initBitapsPayment } = this.props;
+    switch (methodID) {
+      case TopUpMethods.FREEKASSA.id:
+        initBitapsPayment(paymentInfo);
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    const b = block('top-up-layout');
+    const { lang } = this.props;
+    const methodBlocksList = Object.keys(TopUpMethods).map((key, index) =>
+    );
+
+    return (
+      <div className={b}>
+        {methodBlocksList}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    actionProcessing: state.payment.actionProcessing,
+    errorMessage: state.payment.errorMessage,
+    initBitapsPayment: state.payment.qiwiInitSuccess,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopUpLayout);
---
+++ client/app/src/features/payment/TopUpLayout/index.js
@@ -0,0 +1,1 @@
+export default from './TopUpLayout.jsx';
